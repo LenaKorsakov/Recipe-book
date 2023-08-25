ab94169c45fc2be41b6021431f20bbebc4cfa9f0
ElenaKorsakova <lenawwka@gmail.com>
2023-08-25 18:44:36 +0200

    Fix bugs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,7 @@ import { ShoppingListService } from './services/shopping-list.service';
 import { AppRoutingModule } from './app-route.module';
 import { InformationMessageComponent } from './recipes/information-message/information-message.component';
 import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
+import { RecipeService } from './services/recipe.service';
 @NgModule({
   declarations: [
     AppComponent,
@@ -30,7 +31,7 @@ import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component
     RecipeEditComponent,
   ],
   imports: [BrowserModule, FormsModule, AppRoutingModule, ReactiveFormsModule],
-  providers: [ShoppingListService],
+  providers: [ShoppingListService, RecipeService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
